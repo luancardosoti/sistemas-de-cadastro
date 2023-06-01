7cc13d79a154609f94427e087a58fbbc814d3cfc
Luan Cardoso <luancardosoti@gmail.com>
2023-06-01 12:26:44 -0300

    adicionando mais um console no app js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando sistema de gerenciamento de jogos!")
-console.log("Aplicação está se conectando ao banco de dados!")
+console.log("Aplicação está se conectando ao banco de dados!")]
+// mais um console
+console.log("mais um console")
